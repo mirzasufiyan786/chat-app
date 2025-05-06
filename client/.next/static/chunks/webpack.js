f6ac770b1e59cafbab572c3129ec13f5ecf59750
mirzasufiyan786 <ahmadmirzasufiyan@gmail.com>
2025-05-06 16:32:22 +0500

    basic setup is complete
    1.next app.
    2.prisma setup.
    3.node server

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a098246a657e5203"; }
+/******/ 		__webpack_require__.h = function() { return "7b705815c26fc939"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
