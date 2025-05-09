323dd69fe924857064111b256dc4b4a791970f3a
mirzasufiyan786 <ahmadmirzasufiyan@gmail.com>
2025-05-09 16:05:42 +0500

    ....

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "27b931ddd19b884e"; }
+/******/ 		__webpack_require__.h = function() { return "787308d3bfad7aeb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/src/components/Empty.jsx
+++ client/src/components/Empty.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Empty() {
-  return <div>Empty</div>;
+  return <div>Empty...</div>;
 }
 
 export default Empty;
